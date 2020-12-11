efb352c6ce5aa459a12cbe6bf8002a6222b64cf0
Dabin <dorothy9795@gmail.com>
2020-12-11 21:22:58 +0900

    primary key 를 등록하여 차트의 데이터를 얻을 수 있습니다

--- EasyBackEnd/Assets/Scripts/EasyBackEnd/ChartManager.cs
+++ EasyBackEnd/Assets/Scripts/EasyBackEnd/ChartManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using BackEnd;
 using System;
+using System.Linq;
+using LitJson;
+using System.Reflection;
 
 /*
  * 차트 매니저는 뒤끝 version2 기준으로 작성되었습니다.
@@ -13,6 +16,7 @@ namespace BackEndExtends
 {
     public class ChartManager : Singleton<ChartManager>
     {
+        private Dictionary<string, Dictionary<string, Dictionary<object, object>>> dataByKey = new Dictionary<string, Dictionary<string, Dictionary<object, object>>>();
         private Dictionary<string, IList> db = new Dictionary<string, IList>();
         private Dictionary<Type, string> dbNamesByType = new Dictionary<Type, string>();
 
@@ -28,16 +32,23 @@ namespace BackEndExtends
             return (T)db[dbName][id];
         }
 
-        public void LoadChartDatas<T>(string chartName)
+        public T GetDataByKey<T>(string primaryKey, object value, string dbName = null)
+        {
+            dbName = (dbName != null) ? dbName : dbNamesByType[typeof(T)];
+            return (T)dataByKey[dbName][primaryKey][value];
+        }
+
+
+        public void LoadChartDatas<T>(string chartName, params string[] primaryKeys)
         {
             BackendReturnObject returnChartList = Backend.Chart.GetChartList();
             if (returnChartList.IsSuccess())
             {
-                LitJson.JsonData json = returnChartList.GetReturnValuetoJSON();
+                JsonData json = returnChartList.GetReturnValuetoJSON();
                 bool foundedChart = false;
                 for (int i = 0; i < json["rows"].Count; i++)
                 {
-                    LitJson.JsonData row = json["rows"][i];
+                    JsonData row = json["rows"][i];
                     string name = row["chartName"]["S"].ToString();
 
                     if (name == chartName)
@@ -50,7 +61,33 @@ namespace BackEndExtends
 
                             if (returnChartContents.IsSuccess())
                             {
-                                string jsonStr = BackendReturnObject.Flatten(returnChartContents.GetReturnValuetoJSON()["rows"]).ToJson();
+                                if (!dataByKey.ContainsKey(chartName))
+                                    dataByKey.Add(chartName, new Dictionary<string, Dictionary<object, object>>());
+
+                                JsonData dataRows = returnChartContents.GetReturnValuetoJSON()["rows"];
+
+                                if (primaryKeys.Length > 0)
+                                {
+                                    for (int j = 0; j < dataRows.Count; j++)
+                                    {
+                                        JsonData data = dataRows[j];
+                                        for (int k = 0; k < primaryKeys.Length; k++)
+                                        {
+                                            string primaryKey = primaryKeys[k];
+
+                                            if (!dataByKey[chartName].ContainsKey(primaryKey))
+                                                dataByKey[chartName].Add(primaryKey, new Dictionary<object, object>());
+
+                                            string dataJson = BackendReturnObject.Flatten(data).ToJson();
+                                            T toData = JsonUtility.FromJson<T>(dataJson);
+                                            FieldInfo field = typeof(T).GetField(primaryKey);
+                                            object value = field.GetValue(toData);
+                                            dataByKey[chartName][primaryKey].Add(value, toData);
+                                        }
+                                    }
+                                }
+
+                                string jsonStr = BackendReturnObject.Flatten(dataRows).ToJson();
                                 List<T> dataList = JsonToDataList<T>(jsonStr);
 
                                 dbNamesByType.Add(typeof(T), chartName);
--- EasyBackEnd/Assets/Scripts/GameManager.cs
+++ EasyBackEnd/Assets/Scripts/GameManager.cs
@@ -10,8 +10,7 @@ public class GameManager : MonoBehaviour
         EasyBackEnd.Instance.Init(() =>
         {
             //쉽게 게스트 로그인!
-            EasyBackEnd.Instance.GuestLogin();
-
+            Debug.Log("GuestID : " + EasyBackEnd.Instance.GuestLogin());
         }, () =>
         {
             Debug.Log("실패");
@@ -20,11 +19,26 @@ public class GameManager : MonoBehaviour
 
     public void OnClickLoadChartButton()
     {
-        //쉽게 차트 가져오기!
-        ChartManager.Instance.LoadChartDatas<ItemData>("chartExample");
+        //차트 가져오기
+        ChartManager.Instance.LoadChartDatas<ItemData>(
+            chartName: "chartExample",
+            primaryKeys: "itemName" //primaryKey! 등록해놓으면 GetDataByKey 함수로 데이터를 얻을 수 있다
+        );
 
-        //가져온 차트 데이터 사용하기!
-        ItemData item = ChartManager.Instance.GetDataById<ItemData>(0);
-        Debug.Log(item.hpPower);
+        //불러온 차트의 데이터 조회하기
+        List<ItemData> itemDatas = ChartManager.Instance.GetDataList<ItemData>();
+
+        Debug.Log("전체 데이터 조회------");
+        foreach (var data in itemDatas)
+            Debug.Log(data.itemName);
+        Debug.Log("------------------");
+
+        //Primary Key를 이용해서 데이터 접근하기
+        ItemData item1 = ChartManager.Instance.GetDataByKey<ItemData>("itemName", "아이템49");
+        Debug.Log(item1.itemName);
+
+        //가져온 차트의 특정 index 데이터 접근하기
+        ItemData item2 = ChartManager.Instance.GetDataById<ItemData>(2);
+        Debug.Log(item2.itemName);
     }
 }
